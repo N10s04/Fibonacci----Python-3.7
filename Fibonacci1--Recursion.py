8b444d6ac339e747fa4dabdfb90e3bbe3ebaaf2a
Nicholas D. Thoennes <thoennesn@gmail.com>
2018-10-09 12:14:34 -0400

    e

--- Fibonacci1--Recursion.py
+++ Fibonacci1--Recursion.py
@@ -1,8 +1,9 @@
-def fibonacci1 (n) :
-	if n == 1 or n == 2 :
-		return 1
-	return fibonacci1 (n-1) + fibonacci1 (n-2)
+def fib(n):
+    if n == 1 or n == 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
 
-#change the "30" to whatever positive integer you'd like it to calculate up to.
-for i in range (1, 30) :
-	print (fibonacci1 (i))
+
+# change the "30" to whatever positive integer you'd like it to calculate up to.
+for i in range(1, 30):
+    print (fib(i))
--- Fibonacci2--While Loop, Dynamic Assignment.py
+++ Fibonacci2--While Loop, Dynamic Assignment.py
@@ -1,9 +1,10 @@
-def fibonacci2 (n) :
-	a, b = 1, 1
-	for i in range (n-1) :
-		a, b = b, a+b
-	return a
+def fib(n):
+    a, b = 1, 1
+    for i in range(n - 1):
+        a, b = b, a + b
+    return a
 
-#change the "30" to whatever positive integer you'd like it to calculate up to.
-for i in range (1, 30) :
-	print (fibonacci2 (i))
+
+# change the "30" to whatever positive integer you'd like it to calculate up to.
+for i in range(1, 30):
+    print (fib(i))
